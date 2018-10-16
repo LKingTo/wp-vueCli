ece12ea26e5d5d258b9da4f51a0bf3434bd7dd35
leungkingto <jingtao.820@163.com>
2018-10-16 18:27:07 +0800

    安装vue.js及相关loader、新建入口文件app.vue;

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-console.log('main');
+import Vue from "vue";
+import App from './app.vue';
+
+const root = document.createElement('div');
+document.body.appendChild(root);
+
+new Vue({
+	render: (h) => h(App)
+}).$mount(root);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
 	entry: './src/main.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: "index.js"
-	}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader'
+			}
+		]
+	},
+	plugins: [
+		new VueLoaderPlugin()	//解析vue文件模板的插件
+	]
 };
