a82b35609faeb89fc52160dcd6cb0e37c1f2b82e
leungkingto <jingtao.820@163.com>
2018-10-17 11:23:59 +0800

    配置清空dist文件夹，clean-webpack-plugin;

--- build/webpack.config.build.js
+++ build/webpack.config.build.js
@@ -1,10 +1,15 @@
+const path = require('path');
 const { VueLoaderPlugin } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config.base.js');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const config = merge(baseConfig, {
 	plugins: [
+		new CleanWebpackPlugin(['dist'], {
+			root: path.resolve(__dirname, '../')	//配置文件不在根目录时，需要设置根目录
+		}),
 		new VueLoaderPlugin(),	//解析vue文件模板的插件
 		new HtmlWebpackPlugin({title: 'My vue-cli'})
 	]
--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,8 +1,10 @@
+const path = require('path');
 const webpack = require('webpack');
 const { VueLoaderPlugin } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config.base.js');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const config = merge(baseConfig, {
 	devServer: {
@@ -12,6 +14,9 @@ const config = merge(baseConfig, {
 		//quiet: true //控制台中不输出打包的信息
 	},
 	plugins: [
+		new CleanWebpackPlugin(['dist'], {
+			root: path.resolve(__dirname, '../')	//配置文件不在根目录时，需要设置根目录
+		}),
 		new VueLoaderPlugin(),	//解析vue文件模板的插件
 		new HtmlWebpackPlugin({title: 'My vue-cli'}),
 		new webpack.HotModuleReplacementPlugin()
