305b0bbca4abe95eba6ef8a464e6790f369a0346
leungkingto <jingtao.820@163.com>
2018-10-17 14:38:20 +0800

    打包分离css：extract-text-webpack-plugin

--- build/webpack.config.build.js
+++ build/webpack.config.build.js
@@ -1,25 +1,29 @@
 const path = require('path');
-const { VueLoaderPlugin } = require('vue-loader');
+const {VueLoaderPlugin} = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config.base.js');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const config = merge(baseConfig, {
 	module: {
 		rules: [
 			{
 				test: /\.css$/,
-				use: [
-					'vue-style-loader',
-					'css-loader',
-					{
-						loader: 'postcss-loader',
-						options: {
-							sourceMap: true //启用源映射支持，postcss-loader将使用其他加载器给出的先前源映射并相应地更新它
+				use: ExtractTextPlugin.extract({
+					fallback: 'vue-style-loader',
+					use: [
+						'css-loader',
+						{
+							loader: 'postcss-loader',
+							options: {
+								sourceMap: true //启用源映射支持，postcss-loader将使用其他加载器给出的先前源映射并相应地更新它
+							}
 						}
-					}
-				]
+					]
+				})
 			}
 		]
 	},
@@ -27,8 +31,10 @@ const config = merge(baseConfig, {
 		new CleanWebpackPlugin(['dist'], {
 			root: path.resolve(__dirname, '../')	//配置文件不在根目录时，需要设置根目录
 		}),
+		new ExtractTextPlugin('css/style.[hash:16].css'),
 		new VueLoaderPlugin(),	//解析vue文件模板的插件
-		new HtmlWebpackPlugin({title: 'My vue-cli'})
+		new HtmlWebpackPlugin({title: 'My vue-cli'}),
+		new BundleAnalyzerPlugin()		//打包分析插件
 	]
 });
 
